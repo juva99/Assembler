63b04462153d707af95ae9d322853ed7f617ae56
juva <yuval94190@gmail.com>
2024-07-09 16:51:59 +0300

    initial implementation of preprocess

--- include/preprocess.h
+++ include/preprocess.h
@@ -1,8 +1,20 @@
-//
-// Created by Yuval on 09/07/2024.
-//
-
 #ifndef OPENU_PROJECT_PREPROCESS_H
 #define OPENU_PROJECT_PREPROCESS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../include/macro_table.h"
+#include "../include/utils.h"
+
+int preprocess(char filename[]);
+
+int process_line(char line[], FILE *file, FILE *final_file, MacroTable *macros);
+
+int is_macro(char *line);
+
+
+void handle_macro(char line[], FILE *file, MacroTable *macros);
+
 #endif //OPENU_PROJECT_PREPROCESS_H
+
