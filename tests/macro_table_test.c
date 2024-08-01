9446a26a46e3fd16631d421cdee28937b32c9bce
juva <yuval22599@gmail.com>
2024-08-01 11:06:36 +0300

    added some code example to code container and add_command

--- tests/macro_table_test.c
+++ tests/macro_table_test.c
@@ -1,11 +1,24 @@
 #include "../include/macro_table.h"
 #include "../include/command.h"
+#include "../include/code_container.h"
 #include <stdio.h>
+
+
+void printBinary(unsigned short num) {
+    int i;
+    for (i = sizeof(num) * 8 - 2; i >= 0; i--) {
+        printf("%d", (num & (1u << i)) ? 1 : 0);
+    }
+    printf("\n");
+}
+
 
 int main() {
     cmd_struct *cmd;
+    code_cont *code = create_container();
     MacroTable *table = create_table();
-    char bla[] = "LAB: mov r1,*r2";
+    int ic = 0;
+    char bla[] = "jmp  LOOP  ";
     if (!table) {
         fprintf(stderr, "Failed to create table\n");
         return 1;
@@ -37,6 +50,10 @@ int main() {
     printf("%d\n", cmd->src_method);
     printf("%d\n", cmd->dst_method);
     printf("%d\n", cmd->length);
+    add_command(&code, cmd, &ic);
+    printBinary(code->bin_rep);
+    printBinary((code + 1)->bin_rep);
+    printf("%s", (code + 1)->label);
 
     return 0;
 }
