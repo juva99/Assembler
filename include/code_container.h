7b932bfcb096155c8b30ff4a2dc04ea28a789786
juva <yuval22599@gmail.com>
2024-08-01 11:04:42 +0300

    code_container fixes

--- include/code_container.h
+++ include/code_container.h
@@ -7,8 +7,21 @@ typedef struct code_cont {
     char *label; /* not in use for data*/
 } code_cont;
 
+#include "../include/utils.h"
+#include "../include/command.h"
+
+int write_data(code_cont **container, unsigned short data, int *counter);
+
+int write_data_label(code_cont **container, unsigned short data, int *counter, char *label);
+
 int add_data(code_cont **data, unsigned short val, int *dc);
 
-int expend_memory(code_cont **data, int dc);
+int expend_memory(code_cont **container, int counter);
+
+int add_command(code_cont **code, cmd_struct *cmd, int *ic);
+
+unsigned short create_method_line(cmd_struct *cmd, int method, char *value, int is_src);
+
+code_cont *create_container();
 
 #endif //CODE_CONTAINER_H
--- src/code_container.c
+++ src/code_container.c
@@ -1,31 +1,44 @@
 #include "../include/code_container.h"
 
-#include <stdlib.h>
 
-
-int add_data(code_cont **data, unsigned short val, int *dc) {
-    int exp_bool;
-
-    exp_bool = expend_memory(data, *dc);
-    if (!exp_bool) {
-        /*error*/
+int write_data(code_cont **container, unsigned short data, int *counter) {
+    int success;
+    if (!expend_memory(container, *counter)) {
+        /* expend error */
         return 0;
     }
-    (*data + *dc)->bin_rep = val;
-    (*dc)++;
+    (*container + *counter)->bin_rep = data;
+    (*counter)++;
     return 1;
 }
 
-int expend_memory(code_cont **data, int dc) {
+int add_data(code_cont **data, unsigned short val, int *dc) {
+    return write_data(data, val, dc);
+}
+
+int write_data_label(code_cont **container, unsigned short data, int *counter, char *label) {
+    if (!write_data(container, data, counter))
+        return 0;
+    (*container + *counter - 1)->label = strduplic(label);
+    return 1;
+}
+
+
+int expend_memory(code_cont **container, int counter) {
     code_cont *temp;
 
-    temp = realloc(*data, (dc + 1) * sizeof(code_cont));
+    temp = realloc(*container, (counter + 1) * sizeof(code_cont));
     if (temp == NULL) {
         /*error*/
         return 0;
     }
-    *data = temp;
+    *container = temp;
     return 1;
 }
 
+code_cont *create_container() {
+    code_cont *temp;
+    temp = malloc(sizeof(code_cont));
+    return temp;
+}
 
