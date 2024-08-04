9941a3fc980e7a3e5e60ba4c0881e25bec3b752c
juva <yuval22599@gmail.com>
2024-08-05 01:15:33 +0300

    fixes to encode string and encode numeric_data

--- src/utils.c
+++ src/utils.c
@@ -345,7 +345,6 @@ int encode_numeric_data(char *line, code_cont **data, int *dc) {
     char curr_token[MAX_LINE_LENGTH];
 
     count = 0;
-    is_negative = 1;
 
     while (extract_next(line, curr_token, ',')) {
         if (*curr_token == '\0') {
@@ -354,7 +353,6 @@ int encode_numeric_data(char *line, code_cont **data, int *dc) {
         i = 0;
         if (*curr_token == '-') {
             /* negative num */
-            is_negative = -1;
             i = 1;
         }
         /* valid number */
@@ -364,7 +362,7 @@ int encode_numeric_data(char *line, code_cont **data, int *dc) {
                 return 0;
             }
         }
-        num = atoi(curr_token) * is_negative;
+        num = atoi(curr_token);
         val = conv_to_ushort(num);
         add_data(data, val, dc);
 
@@ -388,8 +386,8 @@ int encode_string(char *line, code_cont **data, int *dc) {
     }
 
     extract_next(line, curr_token, '\"');
-    while (*curr_token != '\0') {
-        val = conv_to_ushort((int) *curr_token);
+    while (curr_token[count] != '\0') {
+        val = conv_to_ushort((int) curr_token[count]);
         add_data(data, val, dc);
 
         count++;
