910b0610076b363313862778a9fc6675e49de50a
juva <yuval22599@gmail.com>
2024-08-05 01:18:08 +0300

    added first stage to main

--- include/first_stage.h
+++ include/first_stage.h
@@ -6,7 +6,9 @@
 #include <string.h>
 #include "../include/utils.h"
 #include "../include/symbol_table.h"
-#include "../include/special_symbol_list.h"
+#include "../include/symbol_list.h"
 #include "../include/command.h"
+
+int first_stage_process(char *filename);
 
 #endif //OPENU_PROJECT_FIRST_STAGE_H
