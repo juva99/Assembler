fe37367b4057b6ae182158ff9cdbbaeda0a25aa5
juva <yuval22599@gmail.com>
2024-08-07 12:32:40 +0300

    updating binary line based on stored symbol

--- src/second_stage.c
+++ src/second_stage.c
@@ -16,7 +16,12 @@ int second_stage_process(code_cont *data, code_cont *code, SymTable *sym_table, 
     for (i = 0; i < ic; ++i) {
         if ((code + i)->bin_rep == 0) {
             if ((code + i)->label != NULL && find_sym_value(sym_table, (code + i)->label) != -1) {
-                /* check if extern or data*/
+                /* update line based on stored symbol */
+                symbol = get_symbol(sym_table, (code + i)->label);
+                if (!update_line((code + i), symbol)) {
+                    /* error */
+                    continue;
+                }
             } else {
                 /*error*/
             }
