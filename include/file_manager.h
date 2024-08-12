0980bb7a60ed9d6e5d679ce7256a574becb204d2
yuval ovadia <yuvalova12@gmai.com>
2024-08-12 16:27:24 +0300

    line number for code cont +  new symbol error

--- include/file_manager.h
+++ include/file_manager.h
@@ -48,8 +48,8 @@ typedef enum ERROR_IDS {
     ERROR_ID_28 = 28,
     ERROR_ID_29 = 29,
     ERROR_ID_30 = 30,
-    ERROR_ID_31 = 31
-
+    ERROR_ID_31 = 31,
+    ERROR_ID_32 = 32
 } ERROR_IDS;
 
 typedef struct file_struct {
